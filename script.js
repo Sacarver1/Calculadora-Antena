ebb63968f84194d6c54a95396f641edb0ffadcc1
SantiagoGh001 <91085273+SantiagoGh001@users.noreply.github.com>
2024-04-01 23:36:41 -0500

    Update script.js
    -Separar fun
    -Agregar Lienzo

--- script.js
+++ script.js
@@ -14,32 +14,35 @@ function calcularPIRE(potencia, ganancia) {
     return 10 * Math.log10(potencia) + ganancia;
 }
 
-function verificarCriterioAltura(pire, altura) {
-    let criterio = "";
-    if (pire <= 10) {
-        criterio = "Estación base instalada de manera que la parte más baja del sistema irradiante (antena(s)) está a una altura mínima de 2.2 metros por encima del piso de la zona de público en general.";
-        if (altura > 2.2) {
-            criterio += "\nNo cumple el criterio de instalacion, altura menor a 2.2m";
-        }
-    } else if (pire <= 100) {
-        criterio = "La parte más baja del sistema irradiante (antena(s)) está a una altura mínima de 2.5 metros por encima del piso de la zona de público en general.";
-        if (altura > 2.5) {
-            criterio += "\nNo cumple el criterio de instalacion, altura menor a 2.5m";
-        }
-    }
-    return criterio;
-}
 
-function verificarCriterioFrecuencia(pire, frecuencia) {
-    let criterio = "";
-    if (pire <= 100) {
-        if (frecuencia <= 1500) {
-            criterio += "\nDistancia mínima a zonas accesibles al público en general en la dirección del lóbulo principal es de 2 metros";
-        } else {
-            criterio += "\nDistancia mínima a zonas accesibles al público en general en la dirección del lóbulo principal es de 1 metros";
+
+function verificarCriterio(pire, frecuencia,altura) {
+        if (pire <= 10) {
+            criterio = "Estación base instalada de manera que la parte más baja del sistema irradiante (antena(s)) está a una altura mínima de 2.2 metros por encima del piso de la zona de público en general.";
+            if(altura>2.2){
+                criterio+="\n\tNo cumple el criterio de instalacion, altura menor a 2.2m";
+            }
+        
+        } else if (pire <= 100) {
+            criterio = "La parte más baja del sistema irradiante (antena(s)) está a una altura mínima de 2.5 metros por encima del piso de la zona de público en general.\n\
+                        \nLa distancia mínima a zonas accesibles al público en general en la dirección del lóbulo principal es de 2 metros para frecuencias menores a 1500 MHz o de 1 metro para frecuencias mayores o iguales a 1500 MHz.\n\
+                        \n 	Ninguna otra fuente de radiofrecuencia con PIRE por encima de 10 W se encuentra a una distancia de hasta 10 metros en frecuencias menores a 1500 MHz o 5 metros para frecuencias mayores o iguales a 1500 MHz en la dirección del lóbulo principal(2) y una distancia de hasta 2 metros en otras direcciones para cualquier rango de frecuencia(3).";
+            if(altura>2.5){
+                criterio+="\n\tNo cumple el criterio de instalacion, altura menor a 2.5m";
+            }
+            if(frecuencia<=1500){
+                criterio+="\n\tDistancia mínima a zonas accesibles al público en general en la dirección del lóbulo principal es de 2 metros";
+            }else
+                criterio+="\n\tDistancia mínima a zonas accesibles al público en general en la dirección del lóbulo principal es de 1 metros";
+  
         }
-    }
-    return criterio;
+        else {
+            criterio = "La parte más baja radiante del sistema irradiante (antena(s)) está a una altura mínima de Hb metros por encima del piso de la zona de público en general.\n\
+                        \nLa distancia mínima a zonas accesibles al público en general en la dirección del lóbulo principal es de D metros.\n\
+                        \nNo hay otras fuentes de radiofrecuencia con PIRE por encima de 100 W que se encuentren a una distancia de 5*D metros en la dirección del lóbulo principal y dentro de D metros en otras direcciones(4).";
+        }
+
+     return criterio;
 }
 
 function agregarSimulacion(nombre, tipoServicio, altura, patronRadiacion, potencia, ganancia, frecuencia) {
@@ -47,10 +50,8 @@ function agregarSimulacion(nombre, tipoServicio, altura, patronRadiacion, potenc
 
     let pire = calcularPIRE(potencia, ganancia);
 
-    let criterioAltura = verificarCriterioAltura(pire, altura);
-    let criterioFrecuencia = verificarCriterioFrecuencia(pire, frecuencia);
+    let criterio = verificarCriterio(pire, frecuencia,altura);
 
-    let criterio = criterioAltura + criterioFrecuencia;
 
     let simulacionActual = {
         nombre: nombre,
@@ -81,18 +82,92 @@ function mostrarSimulacion(simulacion) {
     lienzo.dataset.simulacionIndex = simulaciones.length;
     document.getElementById("lienzo-container").appendChild(lienzo);
 
-    let antena = document.createElement("div");
-    antena.classList.add("antena");
-    antena.textContent = "Antena: " + simulacion.nombre + " - " + simulacion.tipoServicio + " - Altura: " + simulacion.altura + "m - Patrón de Radiación: " + simulacion.patronRadiacion;
-    lienzo.appendChild(antena);
+    let cardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container");
 
-    let pireElement = document.createElement("p");
-    pireElement.textContent = "PIRE: " + simulacion.pire.toFixed(2) + " dBm";
-    lienzo.appendChild(pireElement);
+    let antenaCard = document.createElement("div");
+    antenaCard.classList.add("card");
 
-    let criterioElement = document.createElement("p");
-    criterioElement.textContent = "Criterio de Evaluación: " + simulacion.criterio;
-    lienzo.appendChild(criterioElement);
+    let cardHeader = document.createElement("div");
+    cardHeader.classList.add("card-header");
+    cardHeader.textContent = simulacion.nombre;
+
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    // Crear elementos <p> para cada atributo, excepto el nombre
+    let tipoServicioInfo = document.createElement("p");
+    tipoServicioInfo.textContent = "Tipo de Servicio: " + simulacion.tipoServicio;
+
+    let alturaInfo = document.createElement("p");
+    alturaInfo.textContent = "Altura: " + simulacion.altura + "m";
+
+    let patronRadiacionInfo = document.createElement("p");
+    patronRadiacionInfo.textContent = "Patrón de Radiación: " + simulacion.patronRadiacion;
+
+    let potenciaInfo = document.createElement("p");
+    potenciaInfo.textContent = "Potencia (W): " + simulacion.potencia;
+
+    let gananciaInfo = document.createElement("p");
+    gananciaInfo.textContent = "Ganancia (dB): " + simulacion.ganancia;
+
+    let frecuenciaInfo = document.createElement("p");
+    frecuenciaInfo.textContent = "Frecuencia (Hz): " + simulacion.frecuencia;
+
+    let pireInfo = document.createElement("p");
+    pireInfo.textContent = "PIRE: " + simulacion.pire.toFixed(2) + " dBm";
+
+
+    // Agregar cada información al cardBody
+    cardBody.appendChild(tipoServicioInfo);
+    cardBody.appendChild(alturaInfo);
+    cardBody.appendChild(patronRadiacionInfo);
+    cardBody.appendChild(potenciaInfo);
+    cardBody.appendChild(gananciaInfo);
+    cardBody.appendChild(frecuenciaInfo);
+    cardBody.appendChild(pireInfo);
+
+    antenaCard.appendChild(cardHeader);
+    antenaCard.appendChild(cardBody);
+
+    cardContainer.appendChild(antenaCard);
+
+    lienzo.appendChild(cardContainer);
+     // Crear el contenedor de la imagen
+    let imagenContainer = document.createElement("div");
+    imagenContainer.classList.add("imagen-container");
+
+    // Crear la imagen
+    let imagen = document.createElement("img");
+    imagen.src = obtenerImagen(simulacion.patronRadiacion);
+    imagen.alt = "Patrón de Radiación";
+
+    imagen.style.width = "300px"; // Cambiar el tamaño según lo necesites
+    imagen.style.height = "auto";
+    imagenContainer.appendChild(imagen);
+
+    // Modificar la posición de la imagen a absolute
+    imagen.style.position = "absolute";
+
+    imagen.style.right = "0"; // Posicionar a la derecha
+    imagen.style.top = "0%"; // Centrar verticalmente
+    imagen.style.transform = "translateX(-250%)"; // Centrar verticalmente
+
+    lienzo.appendChild(imagenContainer);
+}
+
+function obtenerImagen(patronRadiacion) {
+
+    if (patronRadiacion === "direccional") {
+        return "antena_direccional.png";
+    } else if (patronRadiacion === "sectorial") {
+        return "antena_sectorial.png";
+    } else if (patronRadiacion === "no-direccional") {
+        return "antena_nodir.png";
+    } else {
+        return "antenna.png";
+    }
+
 }
 
 document.getElementById("formulario").addEventListener("submit", function (event) {
@@ -111,6 +186,9 @@ document.getElementById("formulario").addEventListener("submit", function (event
 document.getElementById("nueva-simulacion").addEventListener("click", function () {
     mostrarDialogo();
 });
+document.getElementById("nueva-simulacion2").addEventListener("click", function () {
+    mostrarDialogo();
+});
 
 document.getElementById("simulaciones").addEventListener("click", function (event) {
     let simulacionIndex = event.target.dataset.simulacionIndex;
@@ -122,3 +200,6 @@ document.getElementById("simulaciones").addEventListener("click", function (even
         lienzo.style.display = "block";
     }
 });
+
+
+
