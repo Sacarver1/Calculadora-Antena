deca6954122562aad7c99a5d1342bfec4c2806d6
SantiagoGh001 <91085273+SantiagoGh001@users.noreply.github.com>
2024-04-02 00:07:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ function mostrarSimulacion(simulacion) {
 
     imagen.style.right = "0"; // Posicionar a la derecha
     imagen.style.top = "0%"; // Centrar verticalmente
-    imagen.style.transform = "translateX(-10%)"; // Centrar verticalmente
+    imagen.style.transform = "translateX(220%)"; // Centrar verticalmente
 
     lienzo.appendChild(imagenContainer);
 }
